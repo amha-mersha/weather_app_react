f2871d6b8749363edb6a4db3dc820d6046200730
Amha Mersha <amhamersh@gmail.com>
2025-02-08 00:08:38 +0300

    Done with the search bar component.

---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,7 @@
+export default function SearchBar() {
+  return (
+    <div >
+      <input className="bg-gray-600 text-gray-300 px-4 py-2 w-full rounded-rull rounded-full focus:outline-2 focus:outline-gray-400 " type="text" placeholder="Search for city" />
+    </div>
+  )
+}
